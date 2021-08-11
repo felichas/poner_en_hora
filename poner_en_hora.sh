f2d1458e58cf0eb299649e887661c2ff8bba9c36
felichas <felichas@users.noreply.github.com>
2021-08-11 16:04:54 +0200

    inital commit

---
+++ poner_en_hora.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Ajustar reloj del ordenador"
+read -e -p "Año: " -i "2021" year
+read -e -p "Mes: " -i "08" month
+read -e -p "Día: " -i "12" day
+read -e -p "Hora: " hour
+read -e -p "Minuto: " min
+echo "Ajustando reloj a: ${year}/${month}/${day} ${hour}:${min}"
+sudo date --set="${year}${month}${day} ${hour}:${min}:00"
