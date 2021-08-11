4ccb1016c5ee1613a6bc80c24e6c54da91a20c46
felichas <felichas@users.noreply.github.com>
2021-08-11 16:05:34 +0200

    beautify

--- poner_en_hora.sh
+++ poner_en_hora.sh
@@ -6,5 +6,6 @@ read -e -p "Mes: " -i "08" month
 read -e -p "Día: " -i "12" day
 read -e -p "Hora: " hour
 read -e -p "Minuto: " min
+
 echo "Ajustando reloj a: ${year}/${month}/${day} ${hour}:${min}"
 sudo date --set="${year}${month}${day} ${hour}:${min}:00"
